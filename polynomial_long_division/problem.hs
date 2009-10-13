437d2f377825dc73417fdd369111ead32d5fade3
jc00ke <jesse@maynard.(none)>
2009-10-12 21:09:38 -0700

    Looking like we have all polynomials...

--- polynomial_long_division/problem.hs
+++ polynomial_long_division/problem.hs
@@ -9,10 +9,13 @@ normalize 0::x = normalize x
 normalize x = x
 
 
-pld N [] = ([1/0], [1/0])
-pld N D
-	| length N < length D = (0,N)
-	| length N > length D = pld 
-
+pld _ []	= ([1/0], [1/0])
+pld [] _	= ([],[])
+pld n::ns d::ds
+	| length ns < length ds	= ([],n::ns)
+	| otherwise				=	let	q0	= n/d
+								 	r0	= detract n::ns q0 d::ds
+									(q1, r1)	= pld r0 d::ds
+								in	(q0::q1, r1)
 -- TODO
 -- add a polynomial_long_division that calls pld sanitized on input (normalize)
