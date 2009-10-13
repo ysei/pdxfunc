d54c5a5f700cc47d341b3374711cc205b7cb93da
jc00ke <jesse@maynard.(none)>
2009-10-12 20:42:35 -0700

    Just checking in, not working

--- polynomial_long_division/problem.hs
+++ polynomial_long_division/problem.hs
@@ -2,9 +2,17 @@ degree p = length normalize p
 
 
 
--- normalize function: removes trailing zeroes
+-- normalize function: removes leading zeroes
+
 normalize [] = []
+normalize 0::x = normalize x
+normalize x = x
 
-normalize 0::[] = []
 
-normalize x::[] = x::[]
+pld N [] = ([1/0], [1/0])
+pld N D
+	| length N < length D = (0,N)
+	| length N > length D = pld 
+
+-- TODO
+-- add a polynomial_long_division that calls pld sanitized on input (normalize)
